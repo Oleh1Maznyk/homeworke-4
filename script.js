8b383021e5fb09fea90761748fe99f911f1c5ade
Oleh1Maznyk <maznykoleh283@gmail.com>
2025-06-27 20:22:14 +0200

    new code

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
-const button = document.getElementById("myResult")
 const myNumber = document.getElementById("myNumber")
-const square = document.getElementById("square")
-
-const resultSquare = document.getElementById("squareNum")
 
 const result = document.getElementById("result")
 
-const squareNumber = (num) => num * num
+const resultSqare = document.getElementById("sqareNum")
+const resultCube = document.getElementById("cubeNum")
+const resultDiv = document.getElementById("divNum")
+
+const sqareNumber = (num) => num ** 2
+const cubeNumber = (num) => num ** 3
+const divNumber = (num) => num % 5
+
+button.addEventListener("click", function () {
+    const myColors = ["red", "blue", "green", "yellow", "purple"]
+    document.body.style.backgroundColor = myColors;
+})
 
 result.addEventListener("click", function () {
     let num = parseInt(myNumber.value)
-    resultSquare.innerText = `Квадрат числа ${num} = ${squareNumber(num)}`
+    resultSqare.innerText = `Квадрат числа ${num} = ${sqareNumber(num)}`
+    resultCube.innerText = `Куб числа ${num} = ${cubeNumber(num)}`
+    resultDiv.innerText = `Залишок від ділення на 5: ${divNumber(num)}`
 })
