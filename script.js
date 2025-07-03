444dc119203c15961457e5ec6aba04cbe1faf6f3
Oleh1Maznyk <maznykoleh283@gmail.com>
2025-07-03 22:10:23 +0200

    new kod

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-const myNumber = document.getElementById("myNumber")
+const myNumber = document.getElementById("myNum")
+
+const resultSqare = document.getElementById("squareNum")
+const resultCube = document.getElementById("cubeNum")
+const resultDivFive = document.getElementById("divFive")
 
 const result = document.getElementById("result")
 
-const resultSqare = document.getElementById("sqareNum")
-const resultCube = document.getElementById("cubeNum")
-const resultDiv = document.getElementById("divNum")
-const chengeColor = document.getElementById("changeColor")
-
 const sqareNumber = (num) => num ** 2
 const cubeNumber = (num) => num ** 3
-const divNumber = (num) => num % 5
+const divFive = (num) => num % 5
 
 result.addEventListener("click", function () {
     let num = parseInt(myNumber.value)
-    resultSqare.innerText = `Квадрат числа ${num} = ${sqareNumber(num)}`
-    resultCube.innerText = `Куб числа ${num} = ${cubeNumber(num)}`
-    resultDiv.innerText = `Залишок від ділення на 5: ${divNumber(num)}`
+    resultSqare.innerText = `Квадрат: ${sqareNumber(num)}`
+    resultCube.innerText = `Куб: ${cubeNumber(num)}`
+    resultDivFive.innerText = `Залишок від ділення на 5: ${divFive(num)}`
 })
